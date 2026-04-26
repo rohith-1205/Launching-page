1a388800582a5497cd1a6498d923ccd3ff3229b1
Rohith S <rohithsivakumar1205@gmail.com>
2026-04-26 14:02:36 +0530

    stf update

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export const LINKS = {
   iicHub: "https://drive.google.com/file/d/1LRMibScvc6w1S2wZEfM5kSba9t6I6jNb/view?usp=sharing/download",
   gyan: "https://gyan.cb.amrita.edu/",
   newsletter: "YOUR_LINK_3_HERE",
-  stf: "YOUR_LINK_4_HERE",
+  stf: "https://drive.google.com/file/d/1dxyKZYGulS8f-s7gD53N3ePc96J10MXi/view?usp=sharing",
 };
